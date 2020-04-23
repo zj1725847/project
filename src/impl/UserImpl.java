0b132006b1552dc0ccc9734443616fe3b45e0312
zj1725847 <57245745+zj1725847@users.noreply.github.com>
2020-04-23 17:10:43 +0800

    修改了一哈输出

--- src/impl/UserImpl.java
+++ src/impl/UserImpl.java
@@ -3,7 +3,7 @@ package impl;
 public class UserImpl {
 
     public static void main(String[] args) throws Exception {
-    	System.err.println("你是？");
+    	System.err.println("修改了一哈输出");
     	int a = 10;
     	for (int i = 0; i <= a; i++) {
     		System.out.println(i);
