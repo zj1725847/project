a267e08cdfb5c3ce8bfac8382b0c0deb00a8a943
zj1725847 <zj18382272813@163.com>
2020-04-23 17:02:34 +0800

    添加从1到10的输出

--- src/impl/UserImpl.java
+++ src/impl/UserImpl.java
@@ -4,5 +4,10 @@ public class UserImpl {
 
     public static void main(String[] args) throws Exception {
     	System.err.println("你是？");
+    	int a = 10;
+    	for (int i = 0; i <= a; i++) {
+    		System.out.println(i);
+			Thread.sleep(1000);
+		}
     }
 }
