7d7d6622f14072ab9e40013bb3d429c55f1ed437
zj1725847 <zj18382272813@163.com>
2020-04-27 15:23:02 +0800

    修改部

--- src/impl/UserImpl.java
+++ src/impl/UserImpl.java
@@ -4,11 +4,13 @@ public class UserImpl {
 
     public static void main(String[] args) throws Exception {
     	System.err.println("修改了一哈输出");
+		System.err.println("?????????");
+		System.out.println("1212123123");
     	int a = 10;
     	for (int i = 0; i <= a; i++) {
     		System.out.println(i);
-		System.out.println("i="+i);
-		Thread.sleep(1000);
-	}
+			System.out.println("i="+i);
+			Thread.sleep(1000);
+		}
     }
 }
